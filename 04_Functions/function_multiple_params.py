da1b1af07ca52ad05a6822c04f1232539005170f
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-14 22:19:24 +0530

    Create function_multiple_params.py

---
+++ 04_Functions/function_multiple_params.py
@@ -0,0 +1,9 @@
+# Function with multiple parameters
+
+def add_numbers(a, b):
+    result = a + b
+    print("Sum is:", result)
+
+# Calling the function with two values
+add_numbers(5, 3)
+add_numbers(10, 20)
