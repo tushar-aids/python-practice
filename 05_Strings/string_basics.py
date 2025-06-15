36ea31c98d85c0c0557aeb2ec34813ff646d9e67
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-15 21:00:24 +0530

    Create string_basics.py

---
+++ 05_Strings/string_basics.py
@@ -0,0 +1,21 @@
+# Basic string operations
+
+name = "Tushar Pawar"
+
+# 1. Length of string
+print("Length of name:", len(name))
+
+# 2. Accessing characters
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+# 3. String slicing
+print("First name:", name[0:6])     # Tushar
+print("Last name:", name[7:])       # Pawar
+
+# 4. String in upper & lower case
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+# 5. Check if string contains a word
+print("Contains 'Pawar'?", "Pawar" in name)
