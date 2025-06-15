737132a825dab342be93d75c074d034d163489ac
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-15 21:02:07 +0530

    Create palindrome_check.py

---
+++ 05_Strings/palindrome_check.py
@@ -0,0 +1,14 @@
+# Check if a string is a palindrome
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")  # Convert to lowercase and remove spaces
+    return s == s[::-1]             # Compare with reversed string
+
+# Test cases
+word1 = "madam"
+word2 = "Tushar"
+word3 = "nurses run"
+
+print(f"'{word1}' is palindrome?", is_palindrome(word1))
+print(f"'{word2}' is palindrome?", is_palindrome(word2))
+print(f"'{word3}' is palindrome?", is_palindrome(word3))
