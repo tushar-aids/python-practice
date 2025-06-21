9c334428f9b8b972dc9f854c1e9f1567ec97aa50
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-21 23:55:14 +0530

    Create list_iteration.py

---
+++ 07_Lists/list_iteration.py
@@ -0,0 +1,26 @@
+# Iterate over a list using different methods
+
+fruits = ["apple", "banana", "cherry", "date"]
+
+print("Using simple for loop:")
+for fruit in fruits:
+    print(fruit)
+
+print("\nUsing index:")
+for i in range(len(fruits)):
+    print(f"fruits[{i}] = {fruits[i]}")
+
+
+
+# output:
+# Using simple for loop:
+# apple
+# banana
+# cherry
+# date
+
+# Using index:
+# fruits[0] = apple
+# fruits[1] = banana
+# fruits[2] = cherry
+# fruits[3] = date
