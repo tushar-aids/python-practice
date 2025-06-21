7114dd89816e2697cd4bee5e5b904f1f0c313ee4
Tushar pawar <tusharpawar9699@gmail.com>
2025-06-21 23:46:14 +0530

    Create list_basics.py

---
+++ 07_Lists/list_basics.py
@@ -0,0 +1,16 @@
+# Basic list creation and indexing
+
+fruits = ["apple", "banana", "cherry", "date", "fig"]
+
+print("Full list:", fruits)
+print("First element:", fruits[0])
+print("Last element:", fruits[-1])
+print("Middle 3 elements:", fruits[1:4])
+
+
+
+# output:
+# Full list: ['apple', 'banana', 'cherry', 'date', 'fig']
+# First element: apple
+# Last element: fig
+# Middle 3 elements: ['banana', 'cherry', 'date']
