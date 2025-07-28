7fc5d058ef26243becd0dfc77b5fe4ae70e20f55
Tushar pawar <tusharpawar9699@gmail.com>
2025-07-28 22:44:07 +0530

    Update return_function.py

--- 04_Functions/return_function.py
+++ 04_Functions/return_function.py
@@ -1,8 +1,12 @@
 # Function that returns the square of a number
-
 def square(x):
     return x * x
 
 # Call the function and store result
 result = square(4)
 print("Square is:", result)
+
+
+
+output:
+Square is: 16
