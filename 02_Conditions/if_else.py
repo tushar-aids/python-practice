6addb8ad84713cdfe76d8fb604e0bcbb2babb1f9
Tushar pawar <tusharpawar9699@gmail.com>
2025-07-28 20:01:35 +0530

    Update if_else.py

--- 02_Conditions/if_else.py
+++ 02_Conditions/if_else.py
@@ -1,8 +1,13 @@
 # Check if a number is even or odd
 
 num = int(input("Enter a number: "))
-
 if num % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
+
+
+
+output:
+Enter a number: 10
+Even number
